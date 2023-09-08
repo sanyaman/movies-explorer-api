ea901707be6f43e9b5efaca01784777d37246d35
sanyaman <sanyaman@gmail.com>
2023-09-08 22:19:49 +0300

    conrt

--- controllers/users.js
+++ controllers/users.js
@@ -52,6 +52,7 @@ module.exports.login = (req, res, next) => {
           res.cookie('jwt', token, {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
+            sameSite: true,
           });
           res.send({ token });
         }).catch(() => { throw new UNAUTHORIZED('Ошибка в создании токена'); });
