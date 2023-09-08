73db298a8d983d5ff41cd3c0d7b1612ee5a32351
sanyaman <sanyaman@gmail.com>
2023-09-08 23:38:46 +0300

    обратка

--- controllers/users.js
+++ controllers/users.js
@@ -52,7 +52,6 @@ module.exports.login = (req, res, next) => {
           res.cookie('jwt', token, {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
-            sameSite: true,
           });
           res.send({ token });
         }).catch(() => { throw new UNAUTHORIZED('Ошибка в создании токена'); });
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,34 +1,34 @@
-// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-// const allowedCors = [
-//   // 'http://api.movie-hub.nomoreparties.co',
-//   // 'https://api.movie-hub.nomoreparties.co',
-//   // 'http://movie-hub.nomoredomainsicu.ru',
-//   // 'https://movie-hub.nomoredomainsicu.ru',
-//   'http://localhost:3000',
-//   'https://localhost:3000',
-//   'http://localhost:3001',
-//   'https://localhost:3001',
-// ];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+const allowedCors = [
+  'http://api.movie-hub.nomoreparties.co',
+  'https://api.movie-hub.nomoreparties.co',
+  'http://movie-hub.nomoredomainsicu.ru',
+  'https://movie-hub.nomoredomainsicu.ru',
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+];
 
-// // eslint-disable-next-line consistent-return
-// module.exports = (req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const requestHeaders = req.headers['access-control-request-headers'];
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
-//   if (allowedCors.includes(origin)) {
-//     res.header({
-//       'Access-Control-Allow-Origin': origin,
-//       'Access-Control-Allow-Credentials': true,
-//     });
-//   }
+  if (allowedCors.includes(origin)) {
+    res.header({
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Credentials': true,
+    });
+  }
 
-//   if (method === 'OPTIONS') {
-//     res.header({
-//       'Access-Control-Allow-Headers': requestHeaders,
-//       'Access-Control-Allow-Methods': DEFAULT_ALLOWED_METHODS,
-//     });
-//     return res.end();
-//   }
-//   next();
-// };
+  if (method === 'OPTIONS') {
+    res.header({
+      'Access-Control-Allow-Headers': requestHeaders,
+      'Access-Control-Allow-Methods': DEFAULT_ALLOWED_METHODS,
+    });
+    return res.end();
+  }
+  next();
+};
